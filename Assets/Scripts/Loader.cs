c63d4c15db90ecc65d8e557c18e9a6fcba9cf1c2
Artem-Zhilinskiy <Arterm313132@gmail.com>
2023-01-23 20:32:09 +0300

    23.01.2023
    Сделал префабы врагов

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Loader : MonoBehaviour
+    {
+        public GameObject _manager;
+
+        private void Awake()
+        {
+            if (ManagerScript._instance == null)
+            {
+                Instantiate(_manager);
+            }
+        }
+
+    }
+}
