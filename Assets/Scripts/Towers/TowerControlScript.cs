98121fec7f1b5e289713a337d018f9a57468f4a9
Artem-Zhilinskiy <Arterm313132@gmail.com>
2023-02-08 20:46:35 +0300

    08.02.2023
    Сделал стрельбу

--- Assets/Scripts/Towers/TowerControlScript.cs
+++ Assets/Scripts/Towers/TowerControlScript.cs
@@ -10,9 +10,97 @@ namespace TowerDefense
         private float _timeBetweenAttack;
         [SerializeField]
         private float _attackRadius;
+        [SerializeField]
         private ProjectileScript _projectile;
         private Enemy _targetEnemy = null;
         private float _attackCounter;
+        bool _isAttacking = false;
+
+        private void Update()
+        {
+            _attackCounter -= Time.deltaTime;
+
+            if (_targetEnemy == null)
+            {
+                Enemy _nearestEnemy = GetNearestEnemy();
+                if (_nearestEnemy != null && Vector2.Distance(transform.localPosition, _nearestEnemy.transform.localPosition) <= _attackRadius)
+                {
+                    _targetEnemy = _nearestEnemy;
+                }
+            }
+            else
+            {
+                if (_attackCounter <= 0)
+                {
+                    _isAttacking = true;
+                    _attackCounter = _timeBetweenAttack;
+                }
+                else
+                {
+                    _isAttacking = false;
+                }
+            }
+            
+            if (Vector2.Distance(transform.localPosition, _targetEnemy.transform.localPosition) > _attackRadius)
+            {
+                _targetEnemy = null;
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (_isAttacking == true)
+            {
+                Attack();
+            }
+        }
+
+        public void Attack()
+        {
+            _isAttacking = false;
+            ProjectileScript _newProjectile = Instantiate(_projectile) as ProjectileScript;
+            _newProjectile.transform.localPosition = transform.localPosition;
+
+            if (_targetEnemy == null)
+            {
+                Destroy(_newProjectile);
+            }
+            else
+            {
+                //move projectile to enemy
+                StartCoroutine(MoveProjectile(_newProjectile));
+            }
+        }
+
+        private IEnumerator MoveProjectile(ProjectileScript _projectile)
+        {
+            while (GetTargetDistance(_targetEnemy) > 0.20f && _projectile != null && _targetEnemy != null)
+            {
+                var _direction = _targetEnemy.transform.localPosition - transform.localPosition;
+                var _angleDirection = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+                _projectile.transform.rotation = Quaternion.AngleAxis(_angleDirection, Vector3.forward);
+                _projectile.transform.localPosition = Vector2.MoveTowards(_projectile.transform.localPosition, _targetEnemy.transform.localPosition, 5f *Time.deltaTime);
+                yield return null;
+            }
+
+            if (_projectile != null || _targetEnemy == null)
+            {
+                Destroy(_projectile);
+            }
+        }
+
+        private float GetTargetDistance(Enemy _thisEnemy)
+        {
+            if (_thisEnemy == null)
+            {
+                _thisEnemy = GetNearestEnemy();
+                if (_thisEnemy = null)
+                {
+                    return 0f;
+                }
+            }
+            return Mathf.Abs(Vector2.Distance(transform.localPosition, _thisEnemy.transform.localPosition));
+        }
 
         private List<Enemy> GetEnemiesInRange()
         {
@@ -20,7 +108,7 @@ namespace TowerDefense
 
             foreach (Enemy enemy in ManagerScript.Instance.EnemyList)
             {
-                if (Vector2.Distance(transform.position, enemy.transform.position) <= _attackRadius)
+                if (Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <= _attackRadius)
                 {
                     _enemiesInRange.Add(enemy);
                 }
@@ -35,9 +123,9 @@ namespace TowerDefense
 
             foreach (Enemy enemy in GetEnemiesInRange())
             {
-                if (Vector2.Distance(transform.position, enemy.transform.position) < _smallestDistance)
+                if (Vector2.Distance(transform.localPosition, enemy.transform.localPosition) < _smallestDistance)
                 {
-                    _smallestDistance = Vector2.Distance(transform.position, enemy.transform.position);
+                    _smallestDistance = Vector2.Distance(transform.localPosition, enemy.transform.localPosition);
                     _nearestEnemy = enemy;
                 }
             }
